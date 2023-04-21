4278a2a287a615955970d43a1469d6cb32c27d5e
Tammi Nyberg <tamminyberg@gmail.com>
2023-04-21 14:43:32 +0200

    added createOpenLink
    Opens Google Maps in the app

--- App.js
+++ App.js
@@ -1,41 +1,73 @@
-import React from 'react';
-//import React, { Component } from 'react';
-//import openMap from 'react-native-open-maps';
-import MapView from 'react-native-maps';
-import { StyleSheet,  View, Dimensions } from 'react-native';
-import { Button } from 'react-native';
+import React, {Component} from 'react';
+import { AppRegistry, StyleSheet, Text, View, Button } from 'react-native';
+import { createOpenLink } from 'react-native-open-maps';
 
-//sæt start kort til Odense:
-//finder skærmens størrelse
-const {width, height} = Dimensions.get("window");
-//udregninger der gør noget ift. kortets placering på skærm og zoom
-const ASPECT_RATIO = width/height;
-const LATITUDE_DELTA = 0.02;
-const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
-//koordinater for start position
-const INITIAL_POSITION = {
-  latitude:55.368007,
-  longitude:10.343260,
-  latitudeDelta: LATITUDE_DELTA,
-  longitudeDelta: LONGITUDE_DELTA,
- };
-
-export default function App() {
-  
-  return (
-    
-      <View >
-     <MapView style={styles.map} initialRegion={INITIAL_POSITION} />
-     </View>
-
-  );
+export default class example extends Component{
+  render(){
+    return (
+      <View style={styles.container}>
+<Text style={styles.header}>Go to place</Text>
+<Button
+color={'#bdc3c7'}
+onPress={createOpenLink({latitude: 37.865101, longitude:-119.538330})}
+title={"Click to open"}>
+</Button>
+      </View>
+    );
+  }
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor: '#ecf0f1'
   },
-  map: {
-    width: '100%',
-    height: '100%',
-  },
+  header: {
+    fontSize:28,
+    fontWeight:'bold',
+    color:'#ecf0f1'
+  }
 });
+// import React from 'react';
+// //import React, { Component } from 'react';
+// //import openMap from 'react-native-open-maps';
+// import MapView from 'react-native-maps';
+// import { StyleSheet,  View, Dimensions } from 'react-native';
+// import { Button } from 'react-native';
+
+// //sæt start kort til Odense:
+// //finder skærmens størrelse
+// const {width, height} = Dimensions.get("window");
+// //udregninger der gør noget ift. kortets placering på skærm og zoom
+// const ASPECT_RATIO = width/height;
+// const LATITUDE_DELTA = 0.02;
+// const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+// //koordinater for start position
+// const INITIAL_POSITION = {
+//   latitude:55.368007,
+//   longitude:10.343260,
+//   latitudeDelta: LATITUDE_DELTA,
+//   longitudeDelta: LONGITUDE_DELTA,
+//  };
+
+// export default function App() {
+  
+//   return (
+    
+//       <View >
+//      <MapView style={styles.map} initialRegion={INITIAL_POSITION} />
+//      </View>
+
+//   );
+// }
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+//   map: {
+//     width: '100%',
+//     height: '100%',
+//   },
+// });
