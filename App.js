9aedccc19692da1f1b63beb16d42efa6b9603fe2
Tammi Nyberg <tamminyberg@gmail.com>
2023-04-21 09:26:44 +0200

    added mapview

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet,  View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    
+      <View style={styles.container}>
+      <MapView style={styles.map} />
     </View>
+
   );
 }
 
@@ -17,4 +19,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+       width: '100%',
+    height: '100%',
+  }
 });
